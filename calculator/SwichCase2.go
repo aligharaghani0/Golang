deeb7bd88d1afa9bb040d08050d6bc7e0d482a9e
aligharaghani0 <aligharaghani0@gmail.com>
2023-12-18 15:14:31 +0330

    addingt the defualt option to the swich case

--- calculator/SwichCase2.go
+++ calculator/SwichCase2.go
@@ -1,1 +1,54 @@
+// use the defulat in the swich case of the calculator :)
+
+// in this file i learin the simple swich case in the golang :)
+
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	const (
+		pluss      string = "+"
+		Minues     string = "-"
+		Multiplied string = " '*'"
+		sub        string = "/"
+		power      string = "^"
+	)
+	var (
+		fir_opeartion int
+		sec_operation int
+		operation     string
+	)
+	fmt.Println("enter the first operation ")
+	fmt.Scanln(&fir_opeartion)
+	fmt.Println("enter the operatort between the first and secend operator :)( + , - , * , / , ^ )  ")
+	fmt.Scanln(&operation)
+	fmt.Println("enter the secend operation of the calcultation :) ")
+	fmt.Scanln(&sec_operation)
+
+	//use teh simple swich case :
+
+	switch operation {
+	case "+":
+		fmt.Println("output", fir_opeartion+sec_operation)
+
+	case "-":
+		fmt.Println("output", fir_opeartion-sec_operation)
+	case "*":
+		fmt.Println("output", fir_opeartion*sec_operation)
+	case "^":
+		output := math.Pow(float64(fir_opeartion), float64(sec_operation))
+		fmt.Println("output :) ", output)
+	case "/":
+		output := fir_opeartion / sec_operation
+		fmt.Println("output", output)
+	default:
+		fmt.Println(" hey nuuuuub enter the valid operation or fomr :) Nuuuub sag :)  ")
+
+	}
+
+}
