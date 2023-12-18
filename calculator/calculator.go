97997e383c07a8e7abcd536b39061e8f3f543c21
aligharaghani0 <aligharaghani0@gmail.com>
2023-12-18 15:14:09 +0330

    making the project with simple swich case

---
+++ calculator/calculator.go
@@ -0,0 +1,50 @@
+// in this file i learin the simple swich case in the golang :)
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	const (
+		pluss      string = "+"
+		Minues     string = "-"
+		Multiplied string = " '*'"
+		sub        string = "/"
+		power      string = "^"
+	)
+	var (
+		fir_opeartion int
+		sec_operation int
+		operation     string
+	)
+	fmt.Println("enter the first operation ")
+	fmt.Scanln(&fir_opeartion)
+	fmt.Println("enter the operatort between the first and secend operator :)( + , - , * , / , ^ )  ")
+	fmt.Scanln(&operation)
+	fmt.Println("enter the secend operation of the calcultation :) ")
+	fmt.Scanln(&sec_operation)
+
+	//use teh simple swich case :
+
+	switch operation {
+	case "+":
+		fmt.Println("output", fir_opeartion+sec_operation)
+
+	case "-":
+		fmt.Println("output", fir_opeartion-sec_operation)
+	case "*":
+		fmt.Println("output", fir_opeartion*sec_operation)
+	case "^":
+		output := math.Pow(float64(fir_opeartion), float64(sec_operation))
+		fmt.Println("output :) ", output)
+	case "/":
+		output := fir_opeartion / sec_operation
+		fmt.Println("output", output)
+
+	}
+
+}
